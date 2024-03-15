1e2e1f616717605dd5eac1534278ed5e23ad6ca4
ZebraAlgebra <swang3068@gatech.edu>
2024-03-15 18:22:07 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CDA Project Repo - Exploring Clustering-Based Hybrid Recommendation System on the Bookcrossing Dataset
+
+Team members: Garrison M. Winter, Molly E. Bartley, Samuel Wade Wang
+
+This repository will be used to host our code files.
---
+++ scripts/goodreads_download.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# you will need curl and bash to execute this script
+
+curl -O "https://datarepo.eng.ucsd.edu/mcauley_group/gdrive/goodreads/goodreads_book_genres_initial.json.gz"
+curl -O "https://datarepo.eng.ucsd.edu/mcauley_group/gdrive/goodreads/goodreads_books.json.gz"
+
